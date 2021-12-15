a4ccc92497287b9ef48a176fc5db6de4018e59f0
Bruques <89554226+Bruques@users.noreply.github.com>
2021-12-15 07:26:14 -0300

    Delete cursoPython directory

--- cursoPython/aula8.py
+++
@@ -1,9 +0,0 @@
-# Índices
-
-frase = 'Eu amo minha namorada!'
-tamanhoFrase = len(frase) #mostra a quantidade de letras tem na frase
-contador = 0
-
-while contador <= tamanhoFrase:
-    print(frase[contador], contador)
-    contador = contador + 1
